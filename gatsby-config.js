c593a9ce99178fffa09661589b033b867201e646
Eric Lau <eric.lau@adaptechgroup.com>
2021-05-09 22:54:13 -0700

    Fix linkedin initially load huge size issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
     "gatsby-plugin-sharp",
     "@chakra-ui/gatsby-plugin",
     "gatsby-plugin-webpack-bundle-analyser-v2",
+    "gatsby-plugin-fontawesome-css",
     "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",
