bbf076b84635df3d1c13c66ff28ee93645683d55
Eric Lau <eric.lau@adaptechgroup.com>
2021-05-10 01:35:59 -0700

    Use xss to prevent xss attacks

--- src/components/Episode.tsx
+++ src/components/Episode.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import {Box, Heading, HStack, Text} from "@chakra-ui/react";
-// import * as DOMPurify from 'isomorphic-dompurify';
 import * as dayjs from 'dayjs';
 import {Helmet} from 'react-helmet';
+import xss from 'xss';
 
 const Episode = ({title, description, season_number, episode_number, published_at, tags}) => {
     // const {
@@ -23,7 +23,11 @@ const Episode = ({title, description, season_number, episode_number, published_a
         <Box p={5} shadow="md" borderWidth="1px" as={'article'}>
             <Heading as={'h3'} size="l" color={'primary.800'} fontStyle={'italic'}>Season {season_number},
                 Episode {episode_number} {'=>'} {title}</Heading>
-            <Text mt={4} dangerouslySetInnerHTML={{__html: description}}/>
+            <Text mt={4} dangerouslySetInnerHTML={{
+                __html: xss(description, {
+                    whiteList: ['p', 'b', 'i'], stripIgnoreTag: true
+                })
+            }}/>
             <Text as={'i'}>{dayjs(published_at).format('dddd, MMMM D, YYYY')}</Text>
             <div id="buzzsprout-player-8475794"></div>
             <Helmet>
@@ -34,7 +38,7 @@ const Episode = ({title, description, season_number, episode_number, published_a
             <HStack mt={12} spacing={['3px', '12px']}>{tags.split(',').map(t => <Box borderWidth="1px" borderRadius="lg"
                                                                                      overflow="hidden" padding={2}
                                                                                      bg={"primary.bg"} key={t}><Text
-                 fontSize={'xs'} color={'white'}>{t}</Text></Box>)}</HStack>
+                fontSize={'xs'} color={'white'}>{t}</Text></Box>)}</HStack>
 
         </Box>
     )
