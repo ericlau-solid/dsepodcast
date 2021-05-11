7b93248775b9ecbc1041f75eef9d1f9d6128d6b7
Eric Lau <elau@coastappliances.com>
2021-05-10 23:47:37 -0700

    Add defer back to player script

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -7,7 +7,7 @@ const Player = () => {
         <Box w={['90%', '80%', '70%','70%']} as={'aside'} id={'buzzsprout-large-player-1776406'}>
             <Helmet>
                 <script type='text/javascript' charSet='utf-8'
-                        src='https://www.buzzsprout.com/1776406.js?container_id=buzzsprout-large-player-1776406&player=large'></script>
+                        src='https://www.buzzsprout.com/1776406.js?container_id=buzzsprout-large-player-1776406&player=large' async defer></script>
             </Helmet>
         </Box>
     )
